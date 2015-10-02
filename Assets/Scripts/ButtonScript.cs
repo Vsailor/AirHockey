fec73ba01ce8c626965877c97fb4d4bc0247fe09
Ivan <sirius9764@gmail.com>
2015-10-02 11:16:29 +0300

    Added MainMenuScene, CreateGameScene, LobbyScene

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+    public void LoadScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
