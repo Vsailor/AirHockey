15ee8de6616178aa22c64f56c30b3f590c339ff8
Denis Datrax <datrax@mail.ru>
2015-10-05 20:13:33 +0300

    camera rotation and hit sounds added

---
+++ Assets/BallScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+    public AudioSource[] audio;
+    
+    void Start()
+    {
+        audio = GetComponents<AudioSource>();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        //    if (collision.relativeVelocity.magnitude > 2)
+        if (collision.gameObject.name == "TableBorders")
+        {
+            audio[0].Play();
+        }
+        if (collision.gameObject.name == "Stick")
+        {
+            audio[1].Play();
+        }
+
+    }
+}
---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using CnControls;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform target;
+    public float scrollSpeed = 4, zoomMin = 3, zoomMax = 10, speedX = 200, speedY = 200;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = CnInputManager.GetAxis("Horizontal");
+        var y = CnInputManager.GetAxis("Vertical");
+        var xAngle = x * 200 * Time.deltaTime;
+        var yAngle = y * 200 * Time.deltaTime;
+        transform.RotateAround(target.position, target.up, xAngle);
+    }
+}
